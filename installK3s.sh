f4af8218a7b3303579c2d4f87a88b608d2e3e675
mmaksim <tower50@mail.ru>
2023-12-05 00:08:59 +0400

    fix

--- installK3s.sh
+++ installK3s.sh
@@ -11,25 +11,13 @@ sudo echo "staginguser:ArcPassw0rd" | sudo chpasswd
 # Injecting environment variables
 echo '#!/bin/bash' >> vars.sh
 echo $adminUsername:$1 | awk '{print substr($1,2); }' >> vars.sh
-echo $SPN_CLIENT_ID:$2 | awk '{print substr($1,2); }' >> vars.sh
-echo $SPN_CLIENT_SECRET:$3 | awk '{print substr($1,2); }' >> vars.sh
-echo $SPN_TENANT_ID:$4 | awk '{print substr($1,2); }' >> vars.sh
-echo $vmName:$5 | awk '{print substr($1,2); }' >> vars.sh
+echo $token:$2 | awk '{print substr($1,2); }' >> vars.sh
 echo $location:$6 | awk '{print substr($1,2); }' >> vars.sh
-echo $token:$7 | awk '{print substr($1,2); }' >> vars.sh
-echo $logAnalyticsWorkspace:$8 | awk '{print substr($1,2); }' >> vars.sh
-echo $deployBastion:$9 | awk '{print substr($1,2); }' >> vars.sh
 echo $templateBaseUrl:${10} | awk '{print substr($1,2); }' >> vars.sh
 
 sed -i '2s/^/export adminUsername=/' vars.sh
-sed -i '3s/^/export SPN_CLIENT_ID=/' vars.sh
-sed -i '4s/^/export SPN_CLIENT_SECRET=/' vars.sh
-sed -i '5s/^/export SPN_TENANT_ID=/' vars.sh
-sed -i '6s/^/export vmName=/' vars.sh
+sed -i '6s/^/export token=/' vars.sh
 sed -i '7s/^/export location=/' vars.sh
-sed -i '8s/^/export stagingStorageAccountName=/' vars.sh
-sed -i '9s/^/export logAnalyticsWorkspace=/' vars.sh
-sed -i '10s/^/export deployBastion=/' vars.sh
 sed -i '11s/^/export templateBaseUrl=/' vars.sh
 
 export K3S_VERSION="v1.26.10+k3s2" # Do not change!
