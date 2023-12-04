588323272a15b225e99d908d4ab490977944e9e6
mmaksim <tower50@mail.ru>
2023-12-04 22:29:28 +0400

    fix

--- installK3s.sh
+++ installK3s.sh
@@ -4,9 +4,9 @@ exec >installK3s.log
 sudo apt-get update
 sudo snap install helm --classic
 sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
-sudo export K3S_VERSION="v1.26.10+k3s2" # Do not change!
+export K3S_VERSION="v1.26.10+k3s2" # Do not change!
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=${K3S_VERSION} sh -s - --token $token
-sudo export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 sudo kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.5.4/cert-manager.yaml
 sudo helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
 sudo helm upgrade -i rancher rancher-stable/rancher --create-namespace --namespace cattle-system --set hostname=hklsasddsbxkawd.eastus.cloudapp.azure.com
