38dd5ab43e765335c405ebd2c823a4b92d739cbf
mmaksim <tower50@mail.ru>
2023-12-05 14:03:26 +0400

    fix

--- installK3s.sh
+++ installK3s.sh
@@ -13,46 +13,25 @@ echo '#!/bin/bash' >> vars.sh
 echo $adminUsername:$1 | awk '{print substr($1,2); }' >> vars.sh
 echo $token:$2 | awk '{print substr($1,2); }' >> vars.sh
 echo $location:$3 | awk '{print substr($1,2); }' >> vars.sh
-echo $templateBaseUrl:$4 | awk '{print substr($1,2); }' >> vars.sh
+echo $fqdn:$4 | awk '{print substr($1,2); }' >> vars.sh
+echo $templateBaseUrl:$5 | awk '{print substr($1,2); }' >> vars.sh
+
 
 sed -i '2s/^/export adminUsername=/' vars.sh
 sed -i '3s/^/export token=/' vars.sh
 sed -i '4s/^/export location=/' vars.sh
+sed -i '4s/^/export fqdn=/' vars.sh
 sed -i '5s/^/export templateBaseUrl=/' vars.sh
 
 export K3S_VERSION="v1.26.10+k3s2" # Do not change!
 
 chmod +x vars.sh
 . ./vars.sh
-
-# Creating login message of the day (motd)
-sudo curl -v -o /etc/profile.d/welcomeK3s.sh ${templateBaseUrl}artifacts/welcomeK3s.sh
-
-# Syncing this script log to 'jumpstart_logs' directory for ease of troubleshooting
-sudo -u $adminUsername mkdir -p /home/${adminUsername}/jumpstart_logs
-while sleep 1; do sudo -s rsync -a /var/lib/waagent/custom-script/download/0/installK3s.log /home/${adminUsername}/jumpstart_logs/installK3s.log; done &
-
-# Installing Rancher K3s cluster (single control plane)
-echo ""
-publicIp=$(hostname -i)
-sudo mkdir ~/.kube
-
-sudo -u $adminUsername mkdir /home/${adminUsername}/.kube
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-external-ip ${publicIp} --bind-address ${publicIp}" INSTALL_K3S_VERSION=${K3S_VERSION} sh -
-sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-sudo kubectl config rename-context default arcbox-k3s --kubeconfig /etc/rancher/k3s/k3s.yaml
-sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
-sudo cp /etc/rancher/k3s/k3s.yaml /home/${adminUsername}/.kube/config
-sudo cp /etc/rancher/k3s/k3s.yaml /home/${adminUsername}/.kube/config.staging
-sudo chown -R $adminUsername /home/${adminUsername}/.kube/
-sudo chown -R staginguser /home/${adminUsername}/.kube/config.staging
-
-# Installing Helm 3
-sudo snap install helm --classic
-
-echo ""
-echo "Making sure Rancher K3s cluster is ready..."
-echo ""
-sudo kubectl wait --for=condition=Available --timeout=60s --all deployments -A >/dev/null
-sudo kubectl get nodes -o wide | expand | awk 'length($0) > length(longest) { longest = $0 } { lines[NR] = $0 } END { gsub(/./, "=", longest); print "/=" longest "=\\"; n = length(longest); for(i = 1; i <= NR; ++i) { printf("| %s %*s\n", lines[i], n - length(lines[i]) + 1, "|"); } print "\\=" longest "=/" }'
-echo ""
+curl -sfL https://get.rke2.io | INSTALL_K3S_VERSION=v1.25.15+rke2r1 sh -
+systemctl enable rke2-server.service
+systemctl start rke2-server.service
+echo 'PATH=$PATH:/usr/local/bin' >> /etc/profile && echo 'PATH=$PATH:{{rke_dir}}/rke2/bin' >> /etc/profile && source /etc/profile
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.5.4/cert-manager.yaml
+sleep 60
+helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
+helm upgrade -i rancher rancher-stable/rancher --create-namespace --namespace cattle-system --set hostname=$fqdn
