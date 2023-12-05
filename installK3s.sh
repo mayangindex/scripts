a093ef8458bdff0ba5bd1ba7c711a140f20bd40f
mmaksim <tower50@mail.ru>
2023-12-05 15:14:29 +0400

    fix

--- installK3s.sh
+++ installK3s.sh
@@ -4,9 +4,6 @@ exec 2>&1
 
 sudo apt-get update
 
-sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
-sudo adduser staginguser --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
-sudo echo "staginguser:ArcPassw0rd" | sudo chpasswd
 
 # Injecting environment variables
 echo '#!/bin/bash' >> vars.sh
@@ -15,21 +12,21 @@ echo $token:$2 | awk '{print substr($1,2); }' >> vars.sh
 echo $location:$3 | awk '{print substr($1,2); }' >> vars.sh
 echo $fqdn:$4 | awk '{print substr($1,2); }' >> vars.sh
 echo $templateBaseUrl:$5 | awk '{print substr($1,2); }' >> vars.sh
-
-
 sed -i '2s/^/export adminUsername=/' vars.sh
 sed -i '3s/^/export token=/' vars.sh
 sed -i '4s/^/export location=/' vars.sh
-sed -i '4s/^/export fqdn=/' vars.sh
-sed -i '5s/^/export templateBaseUrl=/' vars.sh
-
+sed -i '5s/^/export fqdn=/' vars.sh
+sed -i '6s/^/export templateBaseUrl=/' vars.sh
 export K3S_VERSION="v1.26.10+k3s2" # Do not change!
-
 chmod +x vars.sh
 . ./vars.sh
+ # Creating login message of the day (motd)
+sudo curl -v -o /etc/profile.d/welcomeK3s.sh ${templateBaseUrl}scripts/welcomeK3s.sh
 curl -sfL https://get.rke2.io | INSTALL_K3S_VERSION=v1.25.15+rke2r1 sh -
 systemctl enable rke2-server.service
 systemctl start rke2-server.service
+export KUBECONFIG=/etc/rancher/rke2/rke2/rke2.yaml
+
 echo 'PATH=$PATH:/usr/local/bin' >> /etc/profile && echo 'PATH=$PATH:{{rke_dir}}/rke2/bin' >> /etc/profile && source /etc/profile
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.5.4/cert-manager.yaml
 sleep 60
