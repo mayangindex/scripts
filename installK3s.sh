8024075c4124693dd9c2021db3a4bf22574905df
mmaksim <tower50@mail.ru>
2023-12-05 21:03:33 +0400

    fix

--- installK3s.sh
+++ installK3s.sh
@@ -92,3 +92,4 @@ kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.5
 sleep 160
 helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
 helm upgrade -i rancher rancher-stable/rancher --create-namespace --namespace cattle-system --set hostname=$fqdn
+rm "$0"
