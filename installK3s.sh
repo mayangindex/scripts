11e092c31b60c75b66598cd20360352cbb953e92
mmaksim <tower50@mail.ru>
2023-12-05 15:46:40 +0400

    fix

--- installK3s.sh
+++ installK3s.sh
@@ -6,22 +6,14 @@ sudo apt-get update
 
 
 # Injecting environment variables
-echo '#!/bin/bash' >> vars.sh
-echo $adminUsername:$1 | awk '{print substr($1,2); }' >> vars.sh
-echo $token:$2 | awk '{print substr($1,2); }' >> vars.sh
-echo $location:$3 | awk '{print substr($1,2); }' >> vars.sh
-echo $fqdn:$4 | awk '{print substr($1,2); }' >> vars.sh
-echo $templateBaseUrl:$5 | awk '{print substr($1,2); }' >> vars.sh
-sed -i '2s/^/export adminUsername=/' vars.sh
-sed -i '3s/^/export token=/' vars.sh
-sed -i '4s/^/export location=/' vars.sh
-sed -i '5s/^/export fqdn=/' vars.sh
-sed -i '6s/^/export templateBaseUrl=/' vars.sh
+export adminUsername=$1
+export token=$2
+export location=$3
+export fqdn=$4
+export templateBaseUrl=$5
 export K3S_VERSION="v1.26.10+k3s2" # Do not change!
-chmod +x vars.sh
-. ./vars.sh
  # Creating login message of the day (motd)
-sudo curl -v -o /etc/profile.d/welcomeK3s.sh ${templateBaseUrl}scripts/welcomeK3s.sh
+sudo curl -v -o /etc/profile.d/welcomeK3s.sh ${templateBaseUrl}/welcomeK3s.sh
 curl -sfL https://get.rke2.io | INSTALL_K3S_VERSION=v1.25.15+rke2r1 sh -
 systemctl enable rke2-server.service
 systemctl start rke2-server.service
